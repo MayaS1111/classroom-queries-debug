83600de142812ab46809ae71da0b19116026585f
Maya Sheriff <113066754+MayaS1111@users.noreply.github.com>
2024-06-12 21:32:46 +0000

    fixed post route

--- config/routes.rb
+++ config/routes.rb
@@ -23,9 +23,9 @@ Rails.application.routes.draw do
   
   get("/courses/:path_id", { :controller => "courses", :action => "show" })
 
-  get("/insert_course", { :controller => "courses", :action => "create" })
+  post("/insert_course", { :controller => "courses", :action => "create" })
 
-  get("/courses", { :controller => "courses_controller", :action => "index" })
+  get("/courses", { :controller => "courses", :action => "index" })
   
   get("/modify_course/:path_id", { :controller => "courses", :action => "update" })
 
